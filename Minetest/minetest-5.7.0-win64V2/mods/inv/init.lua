d382a6a9148ee37f155dbb0ee3b5bde96d498a74
alexis041 <93341437+alexis041@users.noreply.github.com>
2023-09-29 16:20:04 +0200

    ajout de minetest avec les mod inventaire et trade

---
+++ Minetest/minetest-5.7.0-win64V2/mods/inv/init.lua
@@ -0,0 +1,121 @@
+local json = minetest.write_json
+
+minetest.register_privilege("inventaire", {
+    description = "donne acces au inventaire"
+})
+
+minetest.register_chatcommand("invt", {
+    description = "enregistre les inventaire des joueurs connecté",
+    params = "",
+    privs = { inventaire = true },
+    func = function(player_name, param)
+        local all_players = minetest.get_connected_players()
+
+        local player_inventories = {}  -- Tableau pour stocker les inventaires de tous les joueurs
+
+        for _, player in ipairs(all_players) do
+            local player_name = player:get_player_name()
+            local inventory = player:get_inventory()
+            local main_inventory = inventory:get_list("main")
+
+            local item_list = {}  -- Tableau pour stocker les noms des objets avec leur quantité
+
+            for _, itemstack in pairs(main_inventory) do
+                if not itemstack:is_empty() then
+                    local item_name = itemstack:get_name()
+                    local item_count = itemstack:get_count()
+                    table.insert(item_list, { name = item_name, count = item_count })
+                end
+            end
+
+            player_inventories[player_name] = item_list
+        end
+
+        if next(player_inventories) then
+            local json_str = json(player_inventories)  -- Convertir la table en chaîne JSON
+
+            -- Enregistrement dans un fichier
+            local file_path = minetest.get_worldpath() .. "/inventories.json"
+            local file = io.open(file_path, "w")
+            if file then
+                file:write(json_str)
+                file:close()
+                return true, "Inventaires des joueurs enregistrés dans inventories.json."
+            else
+                return false, "Impossible d'ouvrir le fichier pour enregistrement."
+            end
+        else
+            return true, "Aucun joueur connecté ou inventaires vides."
+        end
+    end,
+})
+
+
+
+minetest.register_chatcommand("inv", {
+    description = "enregistre les inventaire du joueur en parametres",
+    params = "<player_name>",
+    privs = { inventaire = true },
+    func = function(name, param)
+        local success, message = save_inventory(param)
+        if success then
+            minetest.chat_send_player(name, message)
+        else
+            minetest.chat_send_player(name, "Erreur: " .. message)
+        end
+    end,
+})
+
+
+-- Fonction pour enregistrer l'inventaire du joueur dans un fichier JSON
+local function save_inventory(player_name)
+    local player = minetest.get_player_by_name(player_name)
+    if not player then
+        return false, "Joueur introuvable."
+    end
+    local inventory = player:get_inventory()
+    local main_inventory = inventory:get_list("main")
+    local item_list = {}  -- Tableau pour stocker les noms des objets
+    for _, itemstack in pairs(main_inventory) do
+        if not itemstack:is_empty() then
+            local item_name = itemstack:get_name()
+            local item_count = itemstack:get_count()
+            table.insert(item_list, item_name .. " (" .. item_count .. ")")
+        end
+    end
+    if #item_list > 0 then
+        -- Créez une table avec le nom du joueur et l'inventaire
+        local player_inventory = {
+            player_name = player_name,
+            inventory = item_list
+        }
+        -- Planifiez l'enregistrement dans 5 secondes
+        local json_str = minetest.write_json(player_inventory)  -- Convertir la table en chaîne JSON
+        -- Enregistrement dans un fichier avec le nom du joueur dans le nom du fichier
+        local file_path = minetest.get_worldpath() .. "/inventory_" .. player_name .. ".json"
+        local file = io.open(file_path, "w")
+        if file then
+            file:write(json_str)
+            file:close()
+            return true, "Objets dans l'inventaire du joueur enregistrés dans inventory.json."
+        else
+            return false, "Impossible d'ouvrir le fichier pour enregistrement."
+        end
+    else
+        return true, "L'inventaire du joueur est vide."
+    end
+end
+
+
+
+-- Hook pour gérer lorsqu'un joueur tape/brise un item
+minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
+    local player_name = puncher:get_player_name()
+    save_inventory(player_name)
+end)
+
+-- Hook pour gérer lorsqu'un joueur fabrique un item
+minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv)
+    local player_name = puncher:get_player_name()
+    save_inventory(player_name)
+end)
