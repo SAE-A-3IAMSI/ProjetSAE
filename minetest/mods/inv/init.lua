98a0dd3bb4cca73781a21d1eda8694833e2ea71c
JoraxTV <jojodu34dadada@gmail.com>
2023-10-26 16:51:18 +0200

    Commande pour appeler l'api pour initProducts (?)

--- minetest/mods/inv/init.lua
+++ minetest/mods/inv/init.lua
@@ -76,44 +76,11 @@ local function save_inventory(player_name)
             player_name = player_name,
             inventory = item_list
         }
-<<<<<<< HEAD:Minetest/minetest-5.7.0-win64V2/mods/inv/init.lua
-
-        -- Convertissez la table en JSON
-        local json_str = minetest.write_json(player_inventory)
-
-        -- Définissez le chemin du fichier JSON en local
-        local file_path = minetest.get_worldpath() .. "/inventory_" .. player_name .. ".json"
-        local file = io.open(file_path, "w")
-        if file then
-            file:write(json_str)
-            file:close()
-            
-            -- Exécutez le fichier batch afficher.bat
-            local command = 'cmd /c "C:/Users/jojod/SAE-A3/ProjetSAE/Minetest/minetest-5.7.0-win64V2/mods/inv/afficher.bat"'
-            local exit_code = os.execute(command)
-
-            
-            if exit_code == 0 then
-                minetest.log("action", "Script afficher.bat exécuté avec succès.")
-                return true, "Données d'inventaire enregistrées localement dans le fichier, script exécuté avec succès."
-            else
-                minetest.log("error", "Erreur lors de l'exécution du script afficher.bat.")
-                return false, "Erreur lors de l'exécution du script afficher.bat."
-            end
-        else
-            minetest.log("error", "Impossible d'ouvrir le fichier pour enregistrement local.")
-            return false, "Impossible d'ouvrir le fichier pour enregistrement local."
-        end
-    else
-        minetest.log("action", "L'inventaire du joueur est vide.")
-        return true, "L'inventaire du joueur est vide."
-=======
     else
         player_inventory = {
             player_name = player_name,
             inventory = "null"
         }
->>>>>>> af0b8ff2f3aa3be160f4ca0d84a733894b00837a:minetest/mods/inv/init.lua
     end
     -- Convertissez la table en JSON
     local json_str = minetest.write_json(player_inventory)
@@ -138,8 +105,6 @@ local function save_inventory(player_name)
 end
 
 
-<<<<<<< HEAD:Minetest/minetest-5.7.0-win64V2/mods/inv/init.lua
-=======
 
 
 
@@ -196,7 +161,6 @@ minetest.register_chatcommand("inv", {
 
 
 
->>>>>>> af0b8ff2f3aa3be160f4ca0d84a733894b00837a:minetest/mods/inv/init.lua
 -- -- Hook pour gérer lorsqu'un joueur fais un clique gauche
 -- minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 -- 	local player_name = puncher:get_player_name()
@@ -220,3 +184,28 @@ minetest.register_on_craft(function(itemstack, player, old_craft_grid, craft_inv
 	local player_name = player:get_player_name()
 	save_inventory(player_name)
 end)
+
+minetest.register_chatcommand("php", {
+    description = "test php",
+    params = "",
+    privs = { inventaire = true },
+    func = function(name, param)
+        local url = "http://api/initProducts.php"
+        local receive_interval = 10
+        local function fetch_callback(res)
+            if not res.completed then
+                minetest.log("error", "Pas de résultat.")
+            end
+            minetest.log("action", res.data)
+        end
+
+        if http_api then
+            http_api.fetch({
+                url = url,
+                method = "POST",
+                data = "test",
+                timeout = receive_interval
+            }, fetch_callback)
+        end
+    end,
+})
