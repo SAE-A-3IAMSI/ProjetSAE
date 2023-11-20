a89f5db6e36b928202d02b213ff962dd54c8ed83
JoraxTV <jojodu34dadada@gmail.com>
2023-11-20 20:07:09 +0100

    New CreatePric + PRBLM DoliIsEmpty

--- api/html/initProducts.php
+++ api/html/initProducts.php
@@ -18,7 +18,7 @@ function createObject($class, $data)
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-Type: application/json',
         'Accept: application/json',
-        'DOLAPIKEY: 0jTMui5CO7nf0ma59XEf0sdF91lTQ4ZA' // clef à changer
+        'DOLAPIKEY: fcfvx5j1ptLP80I01U2xAZ520yrMQQES' // clef à changer
     ));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
@@ -29,8 +29,8 @@ function createNewProduct($name, $label)
 {
     $data = array(
         'ref' => $name,
-        'label' => $label,
-        'price' => createPrice($name),
+        'label' => $label['french'],
+        'price' => $label['price'],
         'status' => 1,
         'status_buy' => 1
     );
@@ -42,7 +42,7 @@ function initAllProducts($productList)
     $dataTranslate = file_get_contents('productsEN-FR.json');
     $allProductsTranslate = json_decode($dataTranslate, true); // Le deuxième argument permet de retourner un tableau associatif
     foreach ($productList as $entry) {
-        createNewProduct($entry, frenchName($entry, $allProductsTranslate));
+        createNewProduct($entry, frenchNameAndPrice($entry, $allProductsTranslate));
         echo "product: " . $entry . " has been created\n";
     }
 }
@@ -77,22 +77,29 @@ function separateWord($product)
 }
 
 
-function frenchName($searchString, $allProductsTranslate)
+function frenchNameAndPrice($searchString, $allProductsTranslate)
 {
     $separateWord = separateWord($searchString);
     foreach ($allProductsTranslate as $item) {
-    if (isset($item['english']) && $item['english'] === $separateWord) {
-            $englishName = $item['english'];
-            break; // La chaîne a été trouvée, pas besoin de continuer la recherche
+        if (isset($item['english']) && $item['english'] === $separateWord) {
+            // Correspondance trouvée, retourner le nom en français et le prix
+            echo "La chaîne '$searchString' a été trouvée dans le tableau.\n";
+            echo "Le prix est : " . $item['price'] . "\n";
+            return [
+                'french' => $item['french'],
+                'price' => $item['price'],
+            ];
         }
     }
 
-    if (!empty($englishName)) {
-        // echo "Le nom en anglais de '$searchString' est : $englishName\n";
-        return $item['french'];
-    } else {
-        echo "La chaîne '$searchString' n'a pas été trouvée dans le tableau.\n";
-    }
+    // Si la correspondance n'est pas trouvée
+    echo "La chaîne '$searchString' n'a pas été trouvée dans le tableau.\n";
+
+    // Retourner le mot en anglais et le prix par défaut
+    return [
+        'french' => $searchString, // Retourne le mot en anglais en l'absence de traduction française
+        'price' => 1,
+    ];
 }
 
 function removeLastUnderscoreAndChar($input)
@@ -100,104 +107,6 @@ function removeLastUnderscoreAndChar($input)
     return preg_replace('/_[a-zA-Z0-9]$/', '', $input);
 }
 
-function createPrice($name)
-{ // fonction qui retourne le prix d'un item en fonction de son nom
-
-    /*
-     *  lors de craft certain prix on un +1 supplémentaire du prix total des objets craft surtout les outils comme bénéfice au craft
-     */
-
-
-    $prices = [
-        "axe wooden" => 4,
-        "axe stone" => 8,
-        "axe bronze" => 20,
-        "axe steel" => 32,
-        "axe mese" => 302,
-        "axe diamond" => 62,
-        "sword wooden" => 3,
-        "sword stone" => 5.5,
-        "sword bronze" => 13.5,
-        "sword steel" => 21.5,
-        "sword mese" => 201.5,
-        "sword diamond" => 41.5,
-        "hoe wooden" => 4,
-        "hoe stone" => 6,
-        "hoe steel" => 22,
-        "shovel wooden" => 3,
-        "shovel stone" => 5,
-        "shovel bronze" => 8,
-        "shovel steel" => 13,
-        "shovel mese" => 102,
-        "shovel diamond" => 22,
-        "trapdoor steel bar" => 13,
-        "trapdoor steel" => 41,
-        "trapdoor" => 6,
-        "door steel bar" => 19,
-        "door steel" => 61,
-        "door obsidian glass" => 7,
-        "door glass" => 7,
-        "wood" => 0.25,
-        "fence rail" => 0.25,
-        "fence" => 1.25,
-        "gate" => 7,
-        "walls" => 1,
-        "bar flat" => 3,
-        "pane flat" => 0.4,
-        "stick" => 1,
-        "stone" => 2,
-        "coal lump" => 3,
-        "copper lump" => 4,
-        "clay lump" => 2,
-        "iron lump" => 5,
-        "tin lump" => 6,
-        "gold lumb" => 8,
-        "mese crystal fragment" => 11,
-        "mese crystal" => 100,
-        "mese" => 900,
-        "clay brick" => 2,
-        "flint and steel" => 12,
-        "diamondblock" => 180,
-        "golddblock" => 108,
-        "coalblock" => 3,
-        "bronzeblock" => 54,
-        "copperblock" => 45,
-        "copper" => 5,
-        "iron" => 10,
-        "tin" => 7,
-        "bronze" => 6,
-        "gold" => 12,
-        "diamond" => 20,
-        "paper" => 3,
-        "book" => 3,
-        "bookshelf" => 16,
-        "bucket" => 31,
-        "screwdriver" => 12,
-        "torch" => 1,
-        "key" => 8,
-        "binoculars" => 21,
-        "mapping kit" => 27,
-        "bug net" => 6,
-        "boat" => 6,
-        "cart" => 51,
-        "gunpowder" => 1,
-        "tnt stick" => 4,
-        "tnt" => 30,
-        "obsidian shard" => 1,
-        "obsidian glass" => 1,
-        "obsidian" => 9,
-        "chest" => 8,
-        "chest locked" => 18,
-    ];
-
-    foreach ($prices as $key => $value) {
-        if (strpos($name, $key) !== false) {
-            return $value;
-        }
-    }
-    // Default price
-    return 1;
-}
 
 // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 //     $data = json_decode(file_get_contents('php://input'), true);
@@ -215,7 +124,7 @@ function read1Products()
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
     $headers = array(
         "Accept: application/json",
-        "DOLAPIKEY: 0jTMui5CO7nf0ma59XEf0sdF91lTQ4ZA" // clef à changer
+        "DOLAPIKEY: fcfvx5j1ptLP80I01U2xAZ520yrMQQES" // clef à changer
     );
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -228,9 +137,9 @@ function isDolibarrProductListEmpty()
 {
     $data = read1Products();
     if (empty($data)) {
-        return true;
+        return false;
     }
-    return false;
+    return true;
 }
 
 if (isDolibarrProductListEmpty()) {
